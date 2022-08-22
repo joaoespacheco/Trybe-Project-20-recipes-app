1a0c18cadfc23773e5efbb3b339f185d937e2693
João Sousa <joaovitors@outlook.com>
2022-08-22 18:16:15 -0300

    Requisito 10: conclusão do requisito

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,6 +13,42 @@ export default function SearchBar() {
           type="text"
         />
       </label>
+      <label htmlFor="ingredient">
+        Ingredient
+        <input
+          id="ingredient-radio"
+          name="filter"
+          type="radio"
+          data-testid="ingredient-search-radio"
+          value="ingredient"
+        />
+      </label>
+      <label htmlFor="name-radio">
+        Name
+        <input
+          id="name-radio"
+          name="filter"
+          type="radio"
+          data-testid="name-search-radio"
+          value="name"
+        />
+      </label>
+      <label htmlFor="first-letter-radio">
+        First Letter
+        <input
+          id="first-letter-radio"
+          name="filter"
+          type="radio"
+          data-testid="first-letter-search-radio"
+          value="first-letter"
+        />
+      </label>
+      <button
+        data-testid="exec-search-btn"
+        type="button"
+      >
+        Search
+      </button>
     </section>
   );
 }
