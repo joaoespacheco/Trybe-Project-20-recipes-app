5990a0bb2906350cba0cb8e2512b63f40466f913
João Sousa <joaovitors@outlook.com>
2022-08-22 15:46:16 -0300

    Requisito 08: conclusão do requisito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { bool, string } from 'prop-types';
+import { Link } from 'react-router-dom';
 import profile from '../images/profileIcon.svg';
 import search from '../images/searchIcon.svg';
 
 export default function Header({ statusButton, pageTitle }) {
   return (
     <header>
-      <img
-        src={ profile }
-        alt="Ícone de perfil"
-        data-testid="profile-top-btn"
-      />
+      <Link to="/profile">
+        <img
+          src={ profile }
+          alt="Ícone de perfil"
+          data-testid="profile-top-btn"
+        />
+      </Link>
       <h1 data-testid="page-title">{ pageTitle }</h1>
       { statusButton && (
         <img
@@ -21,3 +25,8 @@ export default function Header({ statusButton, pageTitle }) {
     </header>
   );
 }
+
+Header.propTypes = {
+  statusButton: bool,
+  pageTitle: string,
+}.isRequired;
---
+++ src/tests/Header.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import renderWithRouter from './helpers/renderWithRouter';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Verificando o componente Header', () => {
+  it('Verificando se os elementos são renderizados', () => {
+    const { history } = renderWithRouter(<App />);
+    
+    history.push('/foods');
+    
+    const iconProfile = screen.getByTestId('profile-top-btn');
+    const iconSearch = screen.getByTestId('search-top-btn');
+    const pageTitle = screen.getByTestId('page-title');
+
+    expect(iconProfile).toBeInTheDocument();
+    expect(iconSearch).toBeInTheDocument();
+    expect(pageTitle).toBeInTheDocument();
+
+  });
+
+  it('Verificando a funcionabilidade do icon profile', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/foods');
+
+    const iconProfile = screen.getByTestId('profile-top-btn');
+    userEvent.click(iconProfile);
+
+    expect(history.location.pathname).toBe('/profile');
+
+  });
+  // it('', () => {
+
+  // });
+});
