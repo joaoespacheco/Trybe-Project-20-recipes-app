323fa3957fb2c0a05cbdb1dc0afb60e4b96b9308
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-08-23 15:29:48 -0300

    Requisito 19 - Conclusão do requisito

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,18 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import CardDrink from '../components/CardDrink';
+import Recipes from '../components/Recipes';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import RecipeContext from '../context/RecipeContext';
 
 export default function Drinks() {
-  const { statusSearchBar, recipesList } = useContext(RecipeContext);
+  const {
+    statusSearchBar,
+    recipesList,
+    handleFoodsAndDriks,
+  } = useContext(RecipeContext);
+
+  useEffect(() => {
+    handleFoodsAndDriks('drinks', 'name', '');
+  }, [handleFoodsAndDriks]);
+
   return (
     <>
       <Header statusButton pageTitle="Drinks" />
       { statusSearchBar && <SearchBar page="drinks" />}
       <section>
-        { recipesList.length > 1 && <CardDrink />}
+        { recipesList.length > 1 && <Recipes page="drinks" />}
       </section>
       { recipesList.length === 1 && (
         <Redirect to={ `/drinks/${recipesList[0].idDrink}` } />
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,18 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import CardFood from '../components/CardFood';
+import Recipes from '../components/Recipes';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import RecipeContext from '../context/RecipeContext';
 
 export default function Foods() {
-  const { statusSearchBar, recipesList } = useContext(RecipeContext);
+  const {
+    statusSearchBar,
+    recipesList,
+    handleFoodsAndDriks,
+  } = useContext(RecipeContext);
+
+  useEffect(() => {
+    handleFoodsAndDriks('foods', 'name', '');
+  }, [handleFoodsAndDriks]);
+
   return (
     <>
       <Header statusButton pageTitle="Foods" />
       { statusSearchBar && <SearchBar page="foods" />}
       <section>
-        { recipesList.length > 1 && <CardFood />}
+        { recipesList.length > 1 && <Recipes page="foods" />}
       </section>
       { recipesList.length === 1 && (
         <Redirect to={ `/foods/${recipesList[0].idMeal}` } />
