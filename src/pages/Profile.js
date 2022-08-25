b73e2b3b96b7f692a8a4f64d6c11516b09d21f6b
João Sousa <joaovitors@outlook.com>
2022-08-25 17:33:41 -0300

    Requisitos 57, 58 e 59 - conclusão dos requisitos

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,11 +3,34 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
   return (
     <>
       <Header statusButton={ false } pageTitle="Profile" />
       <section>
-        <p>Oi, eu sou o Profile</p>
+        <h1 data-testid="profile-email">{ email }</h1>
+
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+
       </section>
       <Footer />
     </>
