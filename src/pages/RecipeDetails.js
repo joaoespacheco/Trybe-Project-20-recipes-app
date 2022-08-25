3751c3f0c73f67edc2603388cb06bbf433f56d2a
Pedro Jorge <pejomadejefi@gmail.com>
2022-08-25 18:46:40 -0300

    Requisito 32: Conclusão do requisito.

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -111,6 +111,19 @@ export default function RecipeDetails() {
           </button>
         )
       }
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        Compartilhar
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+      >
+        Favoritar
+      </button>
+
     </section>
   );
 }
