c1c11d5e3ce945d2e8f57c03e9d281806c0e0e46
Pedro Jorge <pejomadejefi@gmail.com>
2022-08-25 18:40:59 -0300

    Requisito 31: Conclusão do requisito.

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -7,18 +7,27 @@ import getCockTailApi from '../services/CockTailApi';
 
 export default function RecipeDetails() {
   const [recipe, setRecipe] = useState([]);
-  const [statusDone, setStatusDone] = useState(false);
-  const [statusInProgress, setStatusInProgress] = useState(true);
+  const [statusDone, setStatusDone] = useState([true]);
   const history = useHistory();
   const { location: { pathname } } = history;
   const path = pathname.split('/');
-
+  const progressLocalStorage = localStorage.getItem('inProgressRecipes');
+  const inProgressStorage = progressLocalStorage ? JSON.parse(progressLocalStorage) : {
+    cocktails: {},
+    meals: {},
+  };
+  const verifyStatus = () => {
+    if (path[1] === 'foods') {
+      return !!inProgressStorage.meals[path[2]];
+    }
+    return !!inProgressStorage.cocktails[path[2]];
+  };
+  const statusInProgress = verifyStatus();
   const changeRecipe = (recipeApi) => {
     const recipeEntries = Object.entries(recipeApi);
     const changedEntries = recipeEntries.filter(([chave, valor]) => (
       (valor !== ' ' && valor !== '' && valor) && [chave, valor]
     ));
-
     const moreInfos = changedEntries.filter(([chave, valor]) => {
       let validedInfos = [];
       if (!chave.includes('strIngredient') && !chave.includes('strMeasure')) {
@@ -26,26 +35,20 @@ export default function RecipeDetails() {
       }
       return validedInfos;
     });
-
     const ingredientsArray = changedEntries
       .filter(([chave]) => chave.includes('strIngredient'));
     const mensureArray = changedEntries
       .filter(([chave]) => chave.includes('strMeasure'));
-
     const newRecipe = {
       ingredients: [],
       mensures: [],
     };
-
     const { ingredients, mensures } = newRecipe;
-
     moreInfos.forEach(([chave, valor]) => { newRecipe[chave] = valor; });
     ingredientsArray.forEach((ingredient) => ingredients.push(ingredient[1]));
     mensureArray.forEach((mensure) => mensures.push(mensure[1]));
-
     return newRecipe;
   };
-
   useEffect(() => {
     const getApiRecipe = async () => {
       const recipeResponse = path[1] === 'foods' ? await getMealApi('recipe', path[2])
@@ -56,35 +59,19 @@ export default function RecipeDetails() {
     };
     getApiRecipe();
   }, []);
-
   useEffect(() => {
-    const recipesDone = JSON.parse(localStorage.getItem('doneRecipes'));
-    const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
-    if (inProgressRecipes && (
-      inProgressRecipes.meals[path[2]] || inProgressRecipes.cocktails[path[2]])) {
-      setStatusDone(false);
-      setStatusInProgress(true);
-      console.log('entrou no primeiro if');
-    } else if (recipesDone) {
-      const statusRecipe = recipesDone.some(({ id }) => (
+    const handleStatusDone = () => {
+      const doneLocalStorage = localStorage.getItem('doneRecipes');
+      const doneStorage = doneLocalStorage ? JSON.parse(doneLocalStorage) : [];
+      const newStatus = doneStorage.some(({ id }) => (
         id === recipe.idMeal || id === recipe.idDrink
       ));
-      setStatusDone(!statusRecipe);
-      setStatusInProgress(false);
-    } else {
-      setStatusDone(true);
-      setStatusInProgress(false);
-    }
-  }, [recipe]);
-
-  const handleLocalStorage = () => {
-    const storage = localStorage.getItem('inProgressRecipes');
-    const inProgressStorage = storage ? JSON.parse(storage) : {
-      cocktails: {},
-      meals: {},
+      setStatusDone(!newStatus);
     };
+    handleStatusDone();
+  }, [recipe]);
+  const handleLocalStorage = () => {
     const currentRecipe = [];
-
     if (path[1] === 'foods') {
       const newStorage = {
         ...inProgressStorage,
@@ -95,12 +82,11 @@ export default function RecipeDetails() {
       };
       localStorage.setItem('inProgressRecipes', JSON.stringify(newStorage));
     }
-
     if (path[1] === 'drinks') {
       const newStorage = {
         ...inProgressStorage,
-        cockTails: {
-          ...inProgressStorage.cockTails,
+        cocktails: {
+          ...inProgressStorage.cocktails,
           [recipe.idDrink]: currentRecipe,
         },
       };
@@ -108,7 +94,6 @@ export default function RecipeDetails() {
     }
     history.push(`${pathname}/in-progress`);
   };
-
   return (
     <section>
       { recipe.idMeal && <MealRecipe recipe={ recipe } /> }
@@ -122,22 +107,9 @@ export default function RecipeDetails() {
             style={ { position: 'fixed', bottom: '0' } }
             onClick={ () => handleLocalStorage() }
           >
-            Start Recipe
+            {statusInProgress ? 'Continue Recipe' : 'Start Recipe'}
           </button>
         )
-      }
-      {
-        statusInProgress
-      && (
-        <button
-          type="button"
-          data-testid="start-recipe-btn"
-          style={ { position: 'fixed', bottom: '0' } }
-          onClick={ () => history.push(`${pathname}/in-progress`) }
-        >
-          Continue Recipe
-        </button>
-      )
       }
     </section>
   );
