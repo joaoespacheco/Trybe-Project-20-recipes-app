99d2c765dcb02bf21356cb2866d5653b56f5dd0a
João Sousa <joaovitors@outlook.com>
2022-08-25 17:35:33 -0300

    Requisito 60 - conclusão do requisito

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
   const { email } = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+
   return (
     <>
       <Header statusButton={ false } pageTitle="Profile" />
@@ -13,6 +16,7 @@ export default function Profile() {
         <button
           type="button"
           data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
         >
           Done Recipes
         </button>
