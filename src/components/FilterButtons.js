0f85f45b827831905a8b0cbf9ac8c6150145ef98
Pedro Jorge <pejomadejefi@gmail.com>
2022-08-26 18:53:07 -0300

    Requisito 44: Conclusão do requisito.

---
+++ src/components/FilterButtons.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function FilterButtons() {
+  return (
+    <>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+      </button>
+    </>
+
+  );
+}
--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import FilterButtons from '../components/FilterButtons';
 
 export default function DoneRecipes() {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    const storage = localStorage.getItem('doneRecipes');
+    const data = storage ? JSON.parse(storage) : [];
+    setList(data);
+  }, []);
+
   return (
     <>
       <Header statusButton={ false } pageTitle="Done Recipes" />
       <section>
-        <p>Oi, eu sou o DoneRecipes</p>
+        <FilterButtons />
+        { list.map((item, index) => (
+          <div key={ index }>
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ item.image }
+              alt={ item.name }
+              style={ { width: '150px', padding: '0 10px' } }
+            />
+            <p data-testid={ `${index}-horizontal-top-text` }>{ item.category }</p>
+            <p data-testid={ `${index}-horizontal-name` }>{ item.name }</p>
+            <p data-testid={ `${index}-horizontal-done-date` }>{ item.doneDate }</p>
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-share-btn` }
+            >
+              Compartilhar
+            </button>
+            { item.tags.map((tag, i2) => (
+              <p
+                key={ i2 }
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+              >
+                { tag }
+              </p>
+            )) }
+          </div>
+        )) }
       </section>
     </>
   );
