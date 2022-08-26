8968d59ce26e76589bbb5bd9bc0891051a2bb0db
João Sousa <joaovitors@outlook.com>
2022-08-26 19:21:44 -0300

    Requisito 37 - conclusão parcial

---
+++ src/components/FoodInProgress.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import { objectOf, string, func, bool } from 'prop-types';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+
+export default function FoodInProgress({
+  recipe,
+  pageId,
+  handleClickShare,
+  statusMessage,
+}) {
+  const [buttonFavorite, setButtonFavorite] = useState(false);
+  const { ingredients, mensures } = recipe;
+
+  const handleFavorite = () => {
+    const { idMeal, strArea, strCategory, strMeal, strMealThumb } = recipe;
+    const storage = localStorage.getItem('favoriteRecipes');
+    const favoriteStorage = storage ? JSON.parse(storage) : [];
+    const favoriteStatus = favoriteStorage.some(({ id }) => pageId === id);
+    if (favoriteStatus) {
+      const newFavorites = favoriteStorage.filter(({ id }) => pageId !== id);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorites));
+    } else {
+      const currentFavorite = {
+        id: idMeal,
+        type: 'food',
+        nationality: strArea,
+        category: strCategory,
+        alcoholicOrNot: '',
+        name: strMeal,
+        image: strMealThumb,
+      };
+      localStorage.setItem('favoriteRecipes', JSON.stringify(
+        [...favoriteStorage, currentFavorite],
+      ));
+    }
+    setButtonFavorite(!buttonFavorite);
+  };
+
+  const verifyFavoriteSave = () => {
+    const storage = localStorage.getItem('favoriteRecipes');
+    const favoriteStorage = storage ? JSON.parse(storage) : [];
+    return (favoriteStorage
+      .some(({ id }) => pageId === id) ? blackHeartIcon : whiteHeartIcon);
+  };
+
+  return (
+    <section>
+      {
+        !!recipe && (
+          <>
+            <h1 data-testid="recipe-title">{ recipe.strMeal }</h1>
+
+            <img
+              data-testid="recipe-photo"
+              src={ recipe.strMealThumb }
+              alt={ recipe.strMeal }
+              style={ { width: '150px', padding: '0 10px' } }
+            />
+
+            <button
+              type="button"
+              onClick={ handleClickShare }
+              style={ { background: 'none', border: 'none', cursor: 'pointer' } }
+              data-testid="share-btn"
+            >
+              <img
+                src={ shareIcon }
+                alt="Ícone de compartilhamento"
+              />
+            </button>
+            {statusMessage
+      && (
+        <span>
+          Link copied!
+        </span>
+      )}
+            <button
+              type="button"
+              onClick={ handleFavorite }
+              style={ { background: 'none', border: 'none', cursor: 'pointer' } }
+            >
+              <img
+                src={ verifyFavoriteSave() }
+                alt="Ícone de favoritar"
+                data-testid="favorite-btn"
+              />
+            </button>
+
+            <p data-testid="recipe-category">{ recipe.strCategory }</p>
+
+            <ul data-testid="ingredient-step">
+              {ingredients.map((ingredient, index) => (
+                <li
+                  data-testid={ `${index}-ingredient-step` }
+                  key={ `${index}-ingredient-step` }
+                >
+                  { `${ingredient} - ${mensures[index]}` }
+                </li>
+              ))}
+            </ul>
+
+            <p data-testid="instructions">{ recipe.strInstructions }</p>
+
+            <button
+              type="button"
+              data-testid="finish-recipe-btn"
+            >
+              Finalizar Receita
+            </button>
+
+          </>
+        )
+      }
+
+    </section>
+  );
+}
+
+FoodInProgress.propTypes = {
+  recipe: objectOf(string),
+  pageId: string,
+  handleClickShare: func,
+  statusMessage: bool,
+}.isRequired;
--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,9 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import copy from 'clipboard-copy';
+import DrinkInProgress from '../components/DrinkInProgress';
+import FoodInProgress from '../components/FoodInProgress';
+import getMealApi from '../services/MealApi';
+import getCockTailApi from '../services/CockTailApi';
 
 export default function RecipeInProgress() {
+  const [recipe, setRecipe] = useState([]);
+  const [statusMessage, setStatusMessage] = useState(false);
+
+  const history = useHistory();
+  const { location: { pathname } } = history;
+  const path = pathname.split('/');
+
+  const changeRecipe = (recipeApi) => {
+    const recipeEntries = Object.entries(recipeApi);
+    const changedEntries = recipeEntries.filter(([chave, valor]) => (
+      (valor !== ' ' && valor !== '' && valor) && [chave, valor]
+    ));
+    const moreInfos = changedEntries.filter(([chave, valor]) => {
+      let validedInfos = [];
+      if (!chave.includes('strIngredient') && !chave.includes('strMeasure')) {
+        validedInfos = [chave, valor];
+      }
+      return validedInfos;
+    });
+    const ingredientsArray = changedEntries
+      .filter(([chave]) => chave.includes('strIngredient'));
+    const mensureArray = changedEntries
+      .filter(([chave]) => chave.includes('strMeasure'));
+    const newRecipe = {
+      ingredients: [],
+      mensures: [],
+    };
+    const { ingredients, mensures } = newRecipe;
+    moreInfos.forEach(([chave, valor]) => { newRecipe[chave] = valor; });
+    ingredientsArray.forEach((ingredient) => ingredients.push(ingredient[1]));
+    mensureArray.forEach((mensure) => mensures.push(mensure[1]));
+    return newRecipe;
+  };
+
+  const handleClickShare = () => {
+    setStatusMessage(true);
+    copy(`http://localhost:3000${pathname}`);
+  };
+
+  useEffect(() => {
+    const getApiRecipe = async () => {
+      const recipeResponse = path[1] === 'foods' ? await getMealApi('recipe', path[2])
+        : await getCockTailApi('recipe', path[2]);
+      const getResponse = path[1] === 'foods' ? recipeResponse.meals
+        : recipeResponse.drinks;
+      setRecipe(changeRecipe(getResponse[0]));
+    };
+    getApiRecipe();
+  }, []);
+
   return (
     <section>
-      <p>Oi, eu sou o RecipeInProgress</p>
+      {
+        recipe.idMeal && (
+          <FoodInProgress
+            recipe={ recipe }
+            pageId={ path[2] }
+            statusMessage={ statusMessage }
+            handleClickShare={ handleClickShare }
+          />
+        )
+      }
+      {
+        recipe.idDrink && (
+          <DrinkInProgress
+            recipe={ recipe }
+            pageId={ path[2] }
+            statusMessage={ statusMessage }
+            handleClickShare={ handleClickShare }
+          />
+        )
+      }
     </section>
   );
 }
